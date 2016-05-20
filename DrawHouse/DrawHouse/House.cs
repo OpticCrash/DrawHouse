567b439476aedb7df58baba977474464482c488a
OpticCrash <IvanV.Shyrokyh@livenau.net>
2016-05-20 17:08:00 +0300

    DrawHouse project

---
+++ DrawHouse/DrawHouse/House.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace DrawHouse
+{
+    class House
+    {
+        public int Wall { get; private set; }
+        public int RendSpeed
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    Turtle.Speed = value;
+                }
+            }
+        }
+        private bool cond;
+        public House(int wall)
+        {
+            try
+            {
+                if (wall > 0)
+                {
+                    this.Wall = wall;
+                    Turtle.Speed = 7;
+                }
+                else
+                {
+                    throw new Exception("Length of Wall cannot be negative or zero");
+                }
+                cond = true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void DrawSquare()
+        {
+            try
+            {
+                if (cond)
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        DrawSide(Wall, 90);
+                    }
+                }
+                else
+                {
+                    throw new Exception("Length of WindwSides or WallSides are uncorrect, it cann't be drawable");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+
+        private void DrawSide(int HowMove, int HowTurn)
+        {
+            Turtle.Turn(HowTurn);
+            Turtle.Move(HowMove);
+        }
+
+        public void DrawRoof()
+        {
+            DrawSide(Wall, 30);
+            DrawSide(Wall, 120);
+            Turtle.Turn(30);
+        }
+
+        public void DrawWindw()
+        {
+            try
+            {
+                if (2*Wall < 3)
+                {
+                    throw new Exception("Dividing by zero Exception");
+                }
+                else
+                {
+                    Turtle.PenUp();
+                    Turtle.Move(2*Wall / 3);
+                    DrawSide(Wall / 3, 90);
+                    Turtle.PenDown();
+                    Turtle.Move(Wall / 3);
+                    for (int i = 0; i < 4; i++)
+                    {
+                        DrawSide(Wall / 3, 90);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        
+    }
+}
---
+++ DrawHouse/DrawHouse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawHouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            House h = new House(100);
+            h.RendSpeed = 10;
+            h.DrawSquare();
+            h.DrawRoof();
+            h.DrawWindw();
+            Console.ReadKey();
+        }
+    }
+}
